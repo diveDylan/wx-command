842c53e14944b2dedc8e97428d9aa6bff06bd2e5
曾祥达 <zengxiangda@zhongan.io>
2019-08-03 16:29:44 +0800

    finished copy file

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 #!/usr/bin/env node
 var wxCommander = require('commander')
+var fs = require('fs')
+var path = require('path')
 
 // http://nodejs.cn/api/process.html#process_process_cwd  learn something about process
 wxCommander
   .version('1.0.0', '-v, --version')
   .usage('wxs-command')
   .description('a wx-commander for create components or page files')
- 
 
-// test command
+var fileTypes = ['.js', '.json', '.wxml', '.wxss']
+
+
 wxCommander
-  .command('log', 'a log')
-  .action(function() {
-    console.log(`惊不惊喜，刺不刺激`)
+  .command('create <type>', 'create page or component files')
+  .action(function (name, params) {
+    console.log(name, params)
+    if (params === 'page') {
+      try {
+        fileTypes.map(async(i) => { await fs.copyFileSync(`./files/page/index${i}`, `page${i}`) })
+      } catch(err) {
+        console.error('what fuck: copy failed')
+      }
+    } else {
+      try {
+        fileTypes.map(async(i) => { await fs.copyFileSync(`./files/component/index${i}`, `component${i}`) })
+      } catch(err) {
+        console.error('what fuck: copy failed')
+      }
+    }
   })
   .parse(process.argv)
-
-
 
 
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ a wx-command project
 
 1. learn commander usage
  mac error: https://github.com/tj/commander.js/issues/786
+ blog for commander: https://juejin.im/entry/5ac22fec5188255c887bceb7  https://www.cnblogs.com/cangqinglang/p/10642891.htmlcomponent
